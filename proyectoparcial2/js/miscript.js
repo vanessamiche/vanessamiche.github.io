4ca2414bacf8bc253a96ed38e61cd5b5952b1a99
vanessamiche <47998345+vanessamiche@users.noreply.github.com>
2019-04-04 19:33:25 -0700

    Add files via upload

--- proyectoparcial2/js/miscript.js
+++ proyectoparcial2/js/miscript.js
@@ -1,6 +1,36 @@
-$( document ).ready(function() {
-    $( "#lunes" ).click(function() {
-    	$("#lunes-content").css("display","block");
+// $( document ).ready(function() {
+//     $( "#lunes" ).click(function() {
+//     	$("#lunes-content").css("display","block");
 
-});
-});
+// 	});
+// });
+
+
+// $( document ).ready(function() {
+   
+// var image = new Image();
+// var src;
+
+//     $( "#lunes" ).click(function() {
+//     	// $("#lunes-content").css("display","block");
+//     	// var image = new Image();
+
+//     	src = 'dog.jpg'; //Esta es la variable que contiene la url de una imagen ejemplo, luego puedes poner la que quieras
+//     	image.src = src;
+
+//     	$('#image').empty()
+
+//     	$('#image').append(image);
+//  	});
+
+
+//     $( "#martes" ).click(function() {
+//     	src = 'untou.jpg'; 
+//     	image.src = src;
+
+//     	$('#image').empty()
+//     	$('#image').append(image);
+//  	});
+
+
+// });   
